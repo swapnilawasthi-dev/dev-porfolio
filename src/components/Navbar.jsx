c2c8ea19fc7ce7834b4414f2e6ea3bbe62aa9dc2
swapnilawasthi-dev <swapnilawasthi.dev@gmail.com>
2024-04-02 15:49:25 +0530

    Navbar Changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,14 +7,13 @@ const Navbar = () => {
   return (
     <nav className="mb-20 flex items-center justify-between py-6">
       <div className="flex flex-shrink-0 items-center">
-        <img src={logo} alt="logo" />
+        <img className="mx-2 w-10" src={logo} alt="logo" />
       </div>
       <div className="m-8 flex item-center justify-center gap-4 text-2xl">
         <FaLinkedin/>
         <FaGithub/>
         <FaSquareXTwitter/>
       </div>
-    <div></div>
     </nav>
   )
 }
